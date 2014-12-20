6beb9ddf61493d69a3f550e7531fd1d2d7899877
Daniel Guilak <daniel.guilak@gmail.com>
2014-12-20 13:45:31 -0800

    Fake authentication

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+      # Obviously, wouldn't ever do this in production.
+      # Assumes you've already run the set_db:create_test_user rake task
+      @user = User.find(1)
   end
 end
