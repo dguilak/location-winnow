b587a8fb03ba5c3484bfb6d92369e96ebfe63949
Daniel Guilak <daniel@learnup.com>
2015-09-04 11:32:59 -0700

    more headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,7 @@ private
   def allow_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
   end
 end
