6db91786fb927bfb73dbd5390cd4495589bed3d8
Daniel Guilak <daniel@learnup.com>
2015-09-04 11:03:08 -0700

    Allow-Access-Control headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  before_filter :allow_access_control_headers
+
+private
+
+  def allow_access_control_headers do
+    headers['Access-Control-Allow_origin'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+  end
 end
